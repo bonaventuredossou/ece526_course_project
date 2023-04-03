a76043a6c051209c616800de01d70cce9a2cee13
Bonaventure DOSSOU <femipancrace.dossou@gmail.com>
2023-04-03 17:58:46 -0400

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=ecse_526_baseline_normal
 #SBATCH --gres=gpu:a100l:2
-#SBATCH --cpus-per-gpu=18
-#SBATCH --mem=96G
+#SBATCH --cpus-per-gpu=6
+#SBATCH --mem=48G
 #SBATCH --time=168:00:00         
 #SBATCH --partition=long
 #SBATCH --error=/home/mila/b/bonaventure.dossou/ece526_course_project/slurmerror.txt
